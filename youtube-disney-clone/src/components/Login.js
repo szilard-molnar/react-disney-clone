7b3a916357ca7fee8e688fdb06f28c4cfaa7c58e
szilard-molnar <mszilard86@gmail.com>
2021-07-17 17:08:18 -0400

    home page style done

--- youtube-disney-clone/src/components/Login.js
+++ youtube-disney-clone/src/components/Login.js
@@ -6,6 +6,11 @@ const Login = (props) => {
             <Content>
                 <CTA>
                     <CTALogoOne src='/images/cta-logo-one.svg' alt='3 brands logo'/>
+                    <SignUp>GET ALL THERE</SignUp>
+                    <Description>
+                        Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1. 
+                    </Description>
+                    <CTALogoTwo src='/images/cta-logo-two.png' alt='Includes 5' />
                 </CTA>
                 <BgImage />
             </Content>
@@ -60,6 +65,39 @@ const CTALogoOne = styled.img`
     max-width: 600px;
     min-height: 1px;
     display: block;
+    width: 100%;
+`;
+
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+
+    &:hover {
+        background-color: #0483ee;
+    }
+`;
+
+const Description = styled.p`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 14px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
     width: 100%;
 `;
 
