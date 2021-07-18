9980d6abb24756768a32cc5a492ba8b2b03e9e90
szilard-molnar <mszilard86@gmail.com>
2021-07-18 17:35:36 -0400

    login button added with styles

--- youtube-disney-clone/src/components/Header.js
+++ youtube-disney-clone/src/components/Header.js
@@ -6,6 +6,35 @@ const Header = (props) => {
             <Logo>
                 <img src="/images/logo.svg" alt="disney logo"/>
             </Logo>
+            <NavMenu>
+                <a href="/">
+                    <img src='/images/home-icon.svg' alt='home' />
+                    <span>HOME</span>
+                </a>
+                <a href="/search">
+                    <img src='/images/search-icon.svg' alt='search' />
+                    <span>SEARCH</span>
+                </a>
+                <a href="/watchlist">
+                    <img src='/images/plus-icon.svg' alt='watchlist' />
+                    <span>WATCHLIST</span>
+                </a>
+                <a href="/originals">
+                    <img src='/images/star-icon.svg' alt='originals' />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href="/movies">
+                    <img src='/images/movie-icon.svg' alt='movies' />
+                    <span>MOVIES</span>
+                </a>
+                <a href="/series">
+                    <img src='/images/tv-icon.svg' alt='series' />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <Login>
+                Login
+            </Login>
         </Nav>
     )
 }
@@ -32,11 +61,96 @@ const Logo = styled.a`
     max-height: 70px;
     font-size: 0;
     display: inline-block;
-    
+
     img {
         display: block;
         width: 100%;
     }
 `;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    padding: 0;
+    position: relative;
+    margin: 0;
+    margin-right: auto;
+    margin-left: 25px;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        margin-right: 20px;
+
+        img {
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+            z-index: auto;
+        }
+
+        span {
+            margin-left: 10px;
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            padding: 2px 0;
+            white-space: nowrap;
+            position: relative;
+
+            &:before {
+                background-color: rgb(249, 249, 249);
+                border-radius: 0 0 4px 4px;
+                bottom: -6px;
+                height: 2px;
+                left: 0px;
+                content: "";
+                opacity: 0;
+                position: absolute;
+                right:0px;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                visibility: hidden;
+                width: auto;
+            }
+        }
+
+        &:hover {
+            span:before {
+                transform: scaleX(1);
+                visibility: visible;
+                opacity: 1!important;
+            }
+        }
+    }
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const Login = styled.a`
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    transition: all .2s ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
+`;
+
+
 
 export default Header;
