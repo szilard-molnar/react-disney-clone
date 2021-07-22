7eff0dae6f34c300c4694aaef1ba3fafbe14936a
szilard-molnar <mszilard86@gmail.com>
2021-07-21 22:02:33 -0400

    image wraps added

--- youtube-disney-clone/src/components/Viewers.js
+++ youtube-disney-clone/src/components/Viewers.js
@@ -57,7 +57,7 @@ const Wrap = styled.div`
     cursor: pointer;
     overflow: hidden;
     position: relative;
-    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
     border: 3px solid rgba(249, 249, 249, 0.1);
 
     img {
